c500c4a08db203714eb96541c28c9001576fd238
ganli <ganli@qq.com>
2020-12-02 11:16:13 +0800

    fixed:添加并归排序

--- src/main/java/com/justbon/algorithm/popsort/PopSort.java
+++ src/main/java/com/justbon/algorithm/popsort/PopSort.java
@@ -29,6 +29,14 @@ public class PopSort {
         for(int num:arr){
             System.out.print(num+" ");
         }
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;i<arr.length-1-i;j--){
+                int temp =arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
     }
 
     public static void main(String[] args) {
